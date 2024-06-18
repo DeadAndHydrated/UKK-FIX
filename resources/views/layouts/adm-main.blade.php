2e57f6ffb9b1fae58bb707f24c0be9749eb89bc9
DeadAndHydrated <kaylaprilia03@gmail.com>
2024-06-18 13:41:39 +0700

    fixing again

--- resources/views/layouts/adm-main.blade.php
+++ resources/views/layouts/adm-main.blade.php
@@ -57,7 +57,37 @@
                 Interface
             </div>
 
-           
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Data Induk</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="{{ route('barang.index')}}">Barang</a>
+                        <a class="collapse-item" href="{{ route('kategori.index')}}">Kategori</a>
+                    </div>
+                </div>
+            </li>
+
+            <hr class="sidebar-divider my-0">
+
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                    aria-expanded="true" aria-controls="collapseUtilities">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Transaksi</span>
+                </a>
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="{{ route('barangmasuk.index')}}">Barang Masuk</a>
+                        <a class="collapse-item" href="{{ route('barangkeluar.index')}}">Barang Keluar</a>
+                    </div>
+                </div>
+            </li>
 
 <li class="nav-item">
     <a class="nav-link collapsed" href="/kategori" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
